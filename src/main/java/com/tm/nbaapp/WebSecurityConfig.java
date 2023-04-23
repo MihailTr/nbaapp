fffc6c148037908094de05ed3bd84a312fd90ee1
MihailTr <tmyfly@gmail.com>
2023-04-23 07:16:48 +0300

    add Security

--- src/main/java/com/tm/nbaapp/WebSecurityConfig.java
+++ src/main/java/com/tm/nbaapp/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/login").permitAll()
+                .antMatchers("/users/create").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
